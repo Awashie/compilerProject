27e3a0dd33a9323f6cce44974afd521aa4c5f702
noeTem <noel.temores@sjsu.edu>
2026-05-02 13:05:06 -0700

    main file and gitignore

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    // basic usage check
+    if (argc != 3){
+        cout << "USAGE: ./n2cc INPUT.c OUTPUT" << endl;
+        exit(1);
+    }
+
+    
+
+}
